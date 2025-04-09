05001a9b32c553a12c411880df981a1d9409915b
unknown <juanjosesierram@gmail.com>
2025-04-09 09:45:35 -0500

    Create simple server that uses ServiceB

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function logToConsole(message) {
-  console.log(message);
-}
+import express from 'express';
+import serviceB from 'serviceB';
 
-export default logToConsole;
+const app = express();
+
+// routes for the other service are:
+// .../services/serviceB
+// .../services/serviceC
+app.use('/services', serviceB);
+
+app.get('/', (req, res) => {
+  res.send('service A root');
+});
+
+app.listen(4000, () => {
+  console.log('Server running at http://localhost:4000');
+});
